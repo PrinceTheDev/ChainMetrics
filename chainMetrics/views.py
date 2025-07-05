33bdab5762a1edefdcd1d34c561ed05ff4c7999e
PrinceTheDev <sinbadprince9@gmail.com>
2025-07-06 00:03:18 +0100

    installed and set up required libraries / tools

---
+++ chainMetrics/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import response
+from rest_framework.decorators import api_view
+
+
+
+@api_view(['GET'])
+def chainMetrics(request):
+    pass
+
